fc4138f05861f01910e93d96146254994df70e23
ahmed msakni <ahmed@msakni.com>
2024-04-25 13:40:59 +0100

    added booking logic

--- lib/Cars/Screens/AvailableCarsScreen.dart
+++ lib/Cars/Screens/AvailableCarsScreen.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
@@ -9,37 +10,7 @@ class AvailableCarsScreen extends StatelessWidget {
         title: Text('Available Cars'),
         centerTitle: true,
         backgroundColor: Color(0xFF00aa9b), // Change app bar color
-        actions: [
-          Expanded( // Wrap the search box with Expanded
-            child: Container(
-              margin: EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
-              padding: EdgeInsets.symmetric(horizontal: 16.0),
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(20.0),
-              ),
-              child: Row(
-                children: [
-                  Icon(Icons.search, color: Colors.grey),
-                  SizedBox(width: 8.0),
-                  Expanded(
-                    child: TextField(
-                      decoration: InputDecoration(
-                        hintText: 'Search for cars...',
-                        border: InputBorder.none,
-                      ),
-                      onChanged: (value) {
-                        // Perform search based on input value
-                      },
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ),
-        ],
       ),
-
       body: Container(
         color: Color(0xFF232d4b), // Change body color
         child: StreamBuilder<QuerySnapshot>(
@@ -56,13 +27,17 @@ class AvailableCarsScreen extends StatelessWidget {
               );
             }
             final List<DocumentSnapshot> documents = snapshot.data!.docs;
-            return Expanded(
-              child: ListView.builder(
-                itemCount: documents.length,
-                itemBuilder: (context, index) {
-                  final carData = documents[index].data() as Map<String, dynamic>;
-                  return Padding(
-                    padding: EdgeInsets.all(8.0),
+            return ListView.builder(
+              itemCount: documents.length,
+              itemBuilder: (context, index) {
+                final carData = documents[index].data() as Map<String, dynamic>;
+                return Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: GestureDetector(
+                    onTap: () {
+                      String carID = documents[index].id;
+                      _showBookingDialog(context, carID, carData);
+                    },
                     child: Container(
                       decoration: BoxDecoration(
                         color: Colors.white, // Change box color
@@ -78,24 +53,84 @@ class AvailableCarsScreen extends StatelessWidget {
                       ),
                       child: ListTile(
                         leading: Image.network(
-                          carData['photoUrl'], // Assuming the photoUrl contains the URL of the car's image
-                          width: 60, // Adjust width as needed
-                          height: 60, // Adjust height as needed
+                          carData['photoUrl'],
+                          width: 60,
+                          height: 60,
                           fit: BoxFit.cover,
                         ),
                         title: Text(carData['carType']),
                         subtitle: Text('Serial Number: ${carData['serialNumber']}'),
                         trailing: Text('Seats: ${carData['numberOfSeats']}'),
-                        // You can display more details about the car as needed
                       ),
                     ),
-                  );
-                },
-              ),
+                  ),
+                );
+              },
             );
           },
         ),
       ),
     );
+  }
+
+  void _showBookingDialog(BuildContext context, String carID, Map<String, dynamic> carData) {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: Text('Confirm Booking'),
+          content: Text('Do you want to book this car?'),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: Text('Cancel'),
+            ),
+            TextButton(
+              onPressed: () {
+                Navigator.pop(context);
+                _bookCar(context, carID, carData);
+              },
+              child: Text('Confirm'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+
+  void _bookCar(BuildContext context, String carID, Map<String, dynamic> carData) {
+    final currentUserID = FirebaseAuth.instance.currentUser!.uid;
+    int availableSeats = carData['numberOfSeats'];
+    if (availableSeats > 0) {
+      FirebaseFirestore.instance.collection('bookings').add({
+        'carID': carID,
+        'userID': currentUserID,
+        'timestamp': DateTime.now(),
+      }).then((value) {
+        // Update number of seats for the booked car
+        FirebaseFirestore.instance.collection('cars').doc(carID).update({
+          'numberOfSeats': availableSeats - 1,
+        }).then((_) {
+          ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(content: Text('Car booked successfully')),
+          );
+        }).catchError((error) {
+          ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(content: Text('Failed to update number of seats')),
+          );
+        });
+      }).catchError((error) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text('Failed to book car')),
+        );
+      });
+    } else {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('No seats available')),
+      );
+    }
   }
 }
--- lib/Travelers/AvailableTravalersScreen.dart
+++ lib/Travelers/AvailableTravalersScreen.dart
@@ -2,17 +2,18 @@ import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import '../Chat/core/storage.dart';
 
-
 class AvailableTravelerScreen extends StatefulWidget {
   const AvailableTravelerScreen({Key? key}) : super(key: key);
 
   @override
-  _AvailableTravelerScreenState createState() => _AvailableTravelerScreenState();
+  _AvailableTravelerScreenState createState() =>
+      _AvailableTravelerScreenState();
 }
 
 class _AvailableTravelerScreenState extends State<AvailableTravelerScreen> {
   late List<UserData> travelers = [];
   late String currentUserID;
+  String _searchValue = '';
 
   @override
   void initState() {
@@ -46,52 +47,83 @@ class _AvailableTravelerScreenState extends State<AvailableTravelerScreen> {
         centerTitle: true,
       ),
       body: SafeArea(
-        child: Container(
-          padding: const EdgeInsets.all(16.0),
-          color: const Color(0xFF232d4b),
-          child: ListView.builder(
-            itemCount: travelers.length,
-            itemBuilder: (context, index) {
-              final traveler = travelers[index];
-              return Container(
-                margin: const EdgeInsets.symmetric(vertical: 9),
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Container(
                 decoration: BoxDecoration(
                   color: Colors.white,
-                  borderRadius: BorderRadius.circular(10.0),
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.grey.withOpacity(0.5),
-                      spreadRadius: 2,
-                      blurRadius: 5,
-                      offset: const Offset(0, 3),
-                    ),
-                  ],
+                  borderRadius: BorderRadius.circular(20.0),
                 ),
-                child: ListTile(
-                  leading: CircleAvatar(
-                    radius: 25,
-                    backgroundImage: NetworkImage(traveler.avatarUrl),
+                child: TextField(
+                  decoration: InputDecoration(
+                    hintText: 'Search for travelers...',
+                    prefixIcon: Icon(Icons.search),
+                    border: InputBorder.none,
                   ),
-                  title: Text(
-                    traveler.name,
-                    style: const TextStyle(
-                      fontSize: 16.0,
-                      color: Color(0xFF00aa9b),
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  onTap: () {
-                    // Handle tapping on a traveler
-
+                  onChanged: (value) {
+                    setState(() {
+                      _searchValue = value;
+                    });
                   },
                 ),
-              );
-            },
-          ),
+              ),
+            ),
+            Expanded(
+              child: Container(
+                padding: const EdgeInsets.all(16.0),
+                color: const Color(0xFF232d4b),
+                child: ListView.builder(
+                  itemCount: travelers.length,
+                  itemBuilder: (context, index) {
+                    final traveler = travelers[index];
+                    if (_searchValue.isNotEmpty &&
+                        !traveler.name
+                            .toLowerCase()
+                            .contains(_searchValue.toLowerCase())) {
+                      return SizedBox.shrink(); // Hide if not matched
+                    }
+                    return Container(
+                      margin: const EdgeInsets.symmetric(vertical: 9),
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(10.0),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.grey.withOpacity(0.5),
+                            spreadRadius: 2,
+                            blurRadius: 5,
+                            offset: const Offset(0, 3),
+                          ),
+                        ],
+                      ),
+                      child: ListTile(
+                        leading: CircleAvatar(
+                          radius: 25,
+                          backgroundImage: NetworkImage(traveler.avatarUrl),
+                        ),
+                        title: Text(
+                          traveler.name,
+                          style: const TextStyle(
+                            fontSize: 16.0,
+                            color: Color(0xFF00aa9b),
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        onTap: () {
+                          // Handle tapping on a traveler
+                        },
+                      ),
+                    );
+                  },
+                ),
+              ),
+            ),
+          ],
         ),
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
     );
   }
-
 }
